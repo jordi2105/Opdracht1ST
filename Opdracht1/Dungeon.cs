23d4171bb1b93cbeebc238dcf7460d4992ed2d1b
Jordi Verheul <jordi.verheul@hotmail.com>
2016-04-28 12:48:05 +0200

    beter versie

---
+++ Opdracht1/Dungeon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht1
+{
+    class Dungeon
+    {
+        private Zone[] zones;
+        private int level;
+
+        private Node[] shortestpath(Node u, Node v)
+        {
+            return null;
+        }
+    }
+}
---
+++ Opdracht1/Pack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht1
+{
+    class Pack
+    {
+        private List<Monster> monsters;
+        private Node currentNode;
+
+    }
+}
