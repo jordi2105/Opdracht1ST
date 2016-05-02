ed9711d12757e3c6cf9e7c02fef9149e2c32680d
Jordi Verheul <jordi.verheul@hotmail.com>
2016-05-02 15:22:17 +0200

    project files

--- Opdracht1/Pack.cs
+++ Opdracht1/Pack.cs
@@ -11,5 +11,25 @@ namespace Opdracht1
         private List<Monster> monsters;
         private Node currentNode;
 
+        void pack(int n)
+        {
+            monsters = new List<Monster>();
+            for(int i = 0; i < n; i++)
+            {
+                Monster monster = new Monster();
+                monsters.Add(monster);
+            }
+        }
+
+        void move(Node u)
+        {
+            currentNode = u;
+        }
+
+        void attack(Node x)
+        {
+
+        }
+
     }
 }
