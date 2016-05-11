7ca55970ead62598af7f244ec804578bdaf64f29
Jordy van Londen <jordy.vanlonden@gmail.com>
2016-05-11 12:44:21 +0200

    partially done

--- Opdracht1/ItemSpawner.cs
+++ Opdracht1/ItemSpawner.cs
@@ -8,10 +8,20 @@ namespace Opdracht1
 {
     class ItemSpawner
     {
+        private Random random;
 
         public ItemSpawner(Random random)
         {
+            this.random = random;
+        }
 
+        public void spawn(Dungeon dungeon)
+        {
+            List<Zone> zones = dungeon.getZones();
+
+            foreach (Zone zone in zones) {
+                
+            }
         }
     }
 }
